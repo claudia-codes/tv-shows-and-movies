5151fc5db9cd566b0c774cdda418a64306f98fad
claudia donea <claudia.donea24@gmail.com>
2024-04-12 11:04:01 +0200

    Remove dropdown in details view

--- src/components/MainNavBar.tsx
+++ src/components/MainNavBar.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import { Navbar} from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { selectCurrentMediaType } from "../store/mediaSlice";
 import MediaDropdown from "./MediaDropdown";
+import { useLocation } from "react-router-dom";
 
+const isMediaSelectionEnabled = (pathname: string) => !pathname.includes("details")
 interface Props {}
 const MainNavBar: React.FunctionComponent<Props> = () => {
   const currentMediaType = useSelector(selectCurrentMediaType);
-
+  const { pathname } = useLocation();
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
         <Navbar.Brand>Popular {currentMediaType}</Navbar.Brand>
-        <MediaDropdown mediaType={currentMediaType}></MediaDropdown>
+        {isMediaSelectionEnabled(pathname) && <MediaDropdown mediaType={currentMediaType}></MediaDropdown>}
       </Container>
     </Navbar>
   );
